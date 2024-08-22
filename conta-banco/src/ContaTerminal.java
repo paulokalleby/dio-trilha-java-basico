e3decb540ff91968a82ae023a8fbc40c562f7272
paulokalleby <paulo.devweb@gmail.com>
2024-08-21 23:54:44 -0300

    finalização do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe o nome do cliente:");
+        String nome = scanner.nextLine();
+        
+        System.out.println("Informe o numero da conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Informe a agencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf(
+            "Olá, %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo de %.2f já está disponível para saque.",
+            nome,
+            agencia,
+            conta,
+            saldo
+        );
+
+        scanner.close();
     }
 }
